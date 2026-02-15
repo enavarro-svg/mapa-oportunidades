f70281dfa1ab9209a227dc23a9131f7d6d3a238a
enavarro-svg <enavarro@projectability.net>
2026-02-15 10:13:01 -0500

    Create src/App.jsx with main application component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Main Application</h1>
+            <p>Welcome to the React application!</p>
+        </div>
+    );
+}
+
+export default App;
